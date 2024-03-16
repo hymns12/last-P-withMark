6492c658143773835ee182b5d9b0d4a712df5f91
hymns12 <nickcloudsyinka1@gmail.com>
2024-03-16 05:18:06 -0400

    updata

--- src/connettor/connectToContract.js
+++ src/connettor/connectToContract.js
@@ -1,9 +1,10 @@
 import { ethers } from "ethers";
+import Dotenv from './abi.json'
 
 export const connectToContract = () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-    const contractAddress = "THE_CONTRACT_ADDRESS";
+    const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS; // import contract Adderss form ENV file
     const contractAbi = [ ] // The Abi Of My Contract in here 
 
     const StackingContract = new ethers.Contract(contractAddress, contractAbi, provider.getSiner());
